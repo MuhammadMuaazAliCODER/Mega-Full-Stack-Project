d0b3df1915dd2a0416d813b7ad40b47343b5a73e
MuaazAliC <muaazaliwork@gmail.com>
2025-10-08 17:53:20 +0500

    Logout Error solved

--- src/Middlewears/auth.middlewear.js
+++ src/Middlewears/auth.middlewear.js
@@ -1,25 +1,37 @@
 import { user } from "../models/user.model.js";
 import { ApiError } from "../Utils/apierrors.js";
 import { asynHandler } from "../Utils/asynHandler.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const verifyJWT = asynHandler(async(req,res,next)=>{
-    try {
-        const token = req.cookies?.accessToken || 
-        req.header("Authorization")?.replace("Bearer ","")
-        if(!token){
-            throw new ApiError(401,"Unauthorized Request")
-        }
-        const decodedToken =  await jwt.verify(token,process.env.ACCESS_TOKEN_SECRECT)
-        console.log(decodedToken)
-      const finaluser =   await user.findById(decodedToken?._id).select(" -password -RefreshToken ")
-      if(!finaluser){
-         throw new ApiError (401,"Invalid Access token ");
-      }
-      req.user = finaluser;
-      next()
-    } catch (error) {
-        console.log(error)
-        throw new ApiError(401,error?.message|| "Invalid Access Token ")
+export const verifyJWT = asynHandler(async (req, res, next) => {
+  try {
+  
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Request - No token provided");
     }
-})
+
+   
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+   
+
+   
+    const finalUser = await user
+      .findById(decodedToken?.id)
+      .select("-password -RefreshToken");
+
+    if (!finalUser) {
+      throw new ApiError(401, "Invalid Access token - User not found");
+    }
+
+   
+    req.user = finalUser;
+    next();
+  } catch (error) {
+   
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
